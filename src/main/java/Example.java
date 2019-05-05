05c6c5f052e979c1010aa05b34f4d99282077456
Rajiv Ranjan <rajranja@redhat.com>
2019-05-05 13:16:32 +0530

    addressed the merge issues

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,13 +1,15 @@
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import java.net.InetAddress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
 public class Example {
 
-	@RequestMapping("/springboot")
+	@RequestMapping("/hello-springboot")
 	String home() {
 		StringBuffer message = new StringBuffer();
 		message.append("Hello From: ");
@@ -21,10 +23,7 @@ public class Example {
 		}
 		return message.toString();
 	}
-    @RequestMapping("/hello-springboot")
-    String home() {
-        return "Hello World - v3!";
-    }
+    
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Example.class, args);
