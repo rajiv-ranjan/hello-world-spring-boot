462bbeb35cf252e6a97157b5274b4606c9a482be
Rajiv Ranjan <rajranja@redhat.com>
2019-05-05 12:52:42 +0530

    added code to pull the pod ips

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,19 +1,31 @@
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import java.net.InetAddress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
 public class Example {
 
-    @RequestMapping("/springboot")
-    String home() {
-        return "Hello World - v3!";
-    }
+	@RequestMapping("/springboot")
+	String home() {
+		StringBuffer message = new StringBuffer();
+		message.append("Hello From: ");
+		try {
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(Example.class, args);
-    }
+			InetAddress localhost = InetAddress.getLocalHost();
+			message.append(" POD: "+localhost.getHostAddress().trim());
+			
+		} catch (Exception e) {
+			message.append("ERROR-BLOCK");
+		}
+		return message.toString();
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Example.class, args);
+	}
 
 }
