753cb780163424442fd44635438c171009fd804e
Rajiv Ranjan <mr.rajiv.ranjan@outlook.com>
2019-05-05 02:19:16 -0400

    added another context as hello-springboot

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 @EnableAutoConfiguration
 public class Example {
 
-    @RequestMapping("/springboot")
+    @RequestMapping("/hello-springboot")
     String home() {
         return "Hello World - v3!";
     }
